c639d576ac2b1bad93381339555e5e86569efe56
DESKTOP-1GAFFGV\60599 <605990944@qq.com>
2023-05-31 00:34:19 +0800

    test

---
+++ mt.javaweb/src/main/java/selfaddminus.java
@@ -0,0 +1,10 @@
+public class selfaddminus {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = ++a;
+		int c = 3;
+		int d = --c;
+		System.out.println("自增运算厚的值是" + b);
+		System.out.println("自减运算厚的值是" + d);
+	}
+}
